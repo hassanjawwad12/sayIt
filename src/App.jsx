9275a762567d7ac325adc4b60abbf29c2b8af743
Muhammad Hassan Jawwad <hassanjawwad01@gmail.com>
2023-12-22 11:56:10 +0500

    Added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import Landing from "./Components/Landing";
+import Login from "./Auth/Login";
+import Signup from "./Auth/Signup";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      
-      <p className='text-red-400 text-4xl'>Hello</p>
-    </div>
-  )
-}
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={Landing()} />
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />   
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 
-export default App
+export default App;
+ 
