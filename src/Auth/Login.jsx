a2474b73fc081c848cca3b8955a8f4e7ae0a5ceb
Muhammad Hassan Jawwad <hassanjawwad01@gmail.com>
2023-12-22 14:27:59 +0500

    Added Signup

--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -9,7 +9,7 @@ const[ password, setPassword ] = useState('');
   return (
     <div className='bg-gray-800 flex flex-col items-center justify-center min-h-screen w-full background-image'>
 
-      <div className='bg-[#98CABD] flex flex-col items-center justify-center w-2/5 gap-4 rounded-lg text-black bg-opacity-80 p-4'>
+      <div className='bg-[#98CABD] flex flex-col items-center justify-center lg:w-2/5 w-11/12 gap-4 rounded-lg text-black bg-opacity-80 p-4'>
 
         <p className='text-center font-bold text-3xl italic mt-4'>SayIt</p>
         <p className='text-xl italic'>Where Apparel Meets Opinions</p>
@@ -33,7 +33,9 @@ const[ password, setPassword ] = useState('');
           <button className='bg-[#F84C60] text-black font-semibold p-2 w-[30%] rounded-full hover:bg-red-800 hover:text-white'>Login</button>
           <p>Or</p>
           <p>New on Say it ⁉ </p>
-          <button className='bg-[#F84C60] text-black font-semibold p-2 w-[30%] rounded-full hover:bg-red-800 hover:text-white mb-2'>Sign Up</button>
+          <button
+          onClick={() => (window.location.href = '/signup')}
+           className='bg-[#F84C60] text-black font-semibold p-2 w-[30%] rounded-full hover:bg-red-800 hover:text-white mb-2'>Sign Up</button>
 
       </div>
       
--- src/Auth/Signup.jsx
+++ src/Auth/Signup.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 const Signup = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [email, setEmail] = useState("");
 
-export default Signup
+  return (
+    <div className="bg-gray-800 flex flex-col items-center justify-center min-h-screen w-full background-image">
+      <div className="bg-[#98CABD] flex flex-col items-center justify-center lg:w-2/5 w-11/12 gap-4 rounded-lg text-black bg-opacity-80 p-4">
+      <p className='text-center font-bold text-3xl italic mt-4'>SayIt</p>
+        <p className='text-xl italic'>Where Apparel Meets Opinions</p>
+
+        <p className='text-black text-lg font-semibold'>Username</p>
+        <input
+          className='p-2 rounded-lg w-[80%] bg-transparent text-white font-semibold border border-gray-600 focus:outline-none focus:bg-transparent'
+          type='text'
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <p className='text-black text-lg font-semibold'>Email</p>
+        <input
+          className='p-2 rounded-lg w-[80%] bg-transparent text-white font-semibold border border-gray-600 focus:outline-none focus:bg-transparent'
+          type='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+          
+            <p className='text-black text-lg font-semibold'>Password</p>  
+          <input
+            className='p-2 rounded-lg w-[80%] bg-transparent text-white font-semibold border border-gray-600 focus:outline-none focus:bg-transparent'
+            type='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <p className='text-black text-lg font-semibold'>Confirm Password</p>
+          <input
+            className='p-2 rounded-lg w-[80%] bg-transparent text-white font-semibold border border-gray-600 focus:outline-none focus:bg-transparent'
+            type='password'
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}  
+          />
+           <button className='bg-[#F84C60] text-black font-semibold p-2 md:w-[30%] w-[50%] rounded-full hover:bg-red-800 hover:text-white'>Create Account</button>
+          <p>Or </p>
+          <p>Already have an account💨  </p>
+          <button
+          onClick={() => (window.location.href = '/login')}
+           className='bg-[#F84C60] text-black font-semibold p-2 md:w-[30%] w-[50%] rounded-full hover:bg-red-800 hover:text-white mb-2'>Login</button>
+
+
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
