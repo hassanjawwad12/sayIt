2f7954cd715badabc07bf525f9a5280174229c4a
Muhammad Hassan Jawwad <hassanjawwad01@gmail.com>
2024-03-07 20:05:45 +0500

    added gradient

--- src/Explore/Explore.jsx
+++ src/Explore/Explore.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Explore = () => {
   return (
-    <div className="flex flex-col items-center justify-centr w-full min-h-screen bg-[#010314] py-4">
-      <p className="font-bold text-4xl text-[#8e64ff] mt-10">Explore Clothes</p>
+    <div className="flex flex-col items-center justify-center w-full min-h-screen bg-gradient-to-r from-gray-700 via-gray-900 to-black py-4">
+      <p className="font-bold text-5xl text-[#8e64ff] mt-10">Explore Clothes</p>
 
       <form className="w-1/3 mx-auto mt-10">
         <label
@@ -47,18 +47,18 @@ const Explore = () => {
       </form>
 
       <div className="flex flex-row w-[100%] space-x-8 items-center justify-center px-6 gap-8 mt-10">
-        <div className=" flex flex-col p-2 items-center justify-center rounded-lg shadow bg-gray-800 w-2/6 h-88 ">
+        <div className=" flex flex-col p-2 items-center justify-center rounded-lg shadow bg-gradient-to-r from-gray-400 via-gray-600 to-blue-800 w-2/6 h-88 ">
           <img src="tshirt.png" className="w-[90%]" />
-          <p className="font-bold text-2xl text-[#8e64ff] mb-10">White Shirt</p>
+          <p className="font-bold text-2xl text-black mb-10">White Shirt</p>
         </div>
-        <div className=" flex flex-col p-2 items-center justify-center rounded-lg shadow bg-gray-800 w-2/6 h-88 ">
+        <div className=" flex flex-col p-2 items-center justify-center rounded-lg shadow bg-gradient-to-r from-gray-400 via-gray-600 to-blue-800 w-2/6 h-88 ">
           <img src="tshirt.png" className="w-[90%]" />
-          <p className="font-bold text-2xl text-[#8e64ff] mb-10">White Shirt</p>
+          <p className="font-bold text-2xl text-black mb-10">White Shirt</p>
         </div>
       </div>
-      <div className="mt-8 flex flex-col p-2 items-center justify-center rounded-lg shadow bg-gray-800 w-2/6 h-88 ">
+      <div className="mt-8 flex flex-col p-2 items-center justify-center rounded-lg shadow bg-gradient-to-r from-gray-400 via-gray-600 to-blue-800 w-2/6 h-88 ">
         <img src="tshirt.png" className="w-[90%]" />
-        <p className="font-bold text-2xl text-[#8e64ff] mb-10">White Shirt</p>
+        <p className="font-bold text-2xl text-black mb-10">White Shirt</p>
       </div>
     </div>
   );
